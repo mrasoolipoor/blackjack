c7798852207444b8dafc3e97bf486985302fa289
bootcamp1402 <139433347+bootcamp1402@users.noreply.github.com>
2023-07-14 18:40:03 +0330

    Show All Function

---
+++ Show All.py
@@ -0,0 +1,11 @@
+def show_all(player , dealer):
+    print ("player's is : ")
+    for i in player:
+        print (i)
+    
+    
+    print ("dealer's is : ")
+    for j in dealer:
+        print (j)
+    
+#testd
